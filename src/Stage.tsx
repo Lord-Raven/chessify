92e6fc7f6f54b83b75dd767b309c343646abe137
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-16 16:39:16 -0400

    More formatting.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -177,7 +177,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     }
 
     addSpace(char: string): string {
-        return `<div class='box'>${char}</div>`;
+        return `<div class='box'><svg viewBox='0 0 20 20'><text x='2' y='17'>${char}</text></svg></div>`;
     }
 
     buildRow(contents: string): string {
@@ -206,7 +206,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         fen = fen.substring(0, fen.indexOf(' '));
         let result = `---\n`;
         let lines = fen.split('/');
-        result += `<style>.play-area {width: 100%; padding-bottom: 100%; position: relative;}.chessboard {width: 100%; height: 100%; position: absolute; top: 0; left: 0; background: darkslategray} .row{width: 100%; height: 12.5%; display: flex;} div.box {width: 12.5%; height: 100%; display: flex; align-items: center; justify-content: center; font-size: 100%; font-family: monospace;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
+        result += `<style>.play-area {width: 512px; min-width: 50%; padding-bottom: 100%; position: relative;}.chessboard {width: 100%; height: 100%; position: absolute; top: 0; left: 0; background: darkslategray} .row{width: 100%; height: 12.5%; display: flex;} div.box {width: 12.5%; height: 100%; display: flex; align-items: center; justify-content: center; font-size: 100%; font-family: monospace;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}svg{width:100%;}</style>`;
         result += `<div class='play-area'><div class='chessboard'>`;
         lines.forEach(line => result += this.buildRow(line));
         result += `</div></div>`;
