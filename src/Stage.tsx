99aecc7cb3d1e8ec508f769d03f501f03575cbd9
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-16 16:54:55 -0400

    More formatting.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -14,18 +14,18 @@ type ChatStateType = any;
 const MOVE_REGEX = /([a-hA-H][1-8])/gm
 
 const PIECE_MAPPING: {[key: string]: string} = {
-    "K": `\u265A`,//`<span class='white-piece'>\u265A</span>`, // \u2654
-    "Q": `♚`,//`<span class='white-piece'>\u265B</span>`, //'\u2655',
-    "R": `<span class='white-piece'>\u265C</span>`, //'\u2656',
-    "B": `<span class='white-piece'>\u265D</span>`, //'\u2657',
-    "N": `<span class='white-piece'>\u265E</span>`, //'\u2658',
-    "P": `<span class='white-piece'>\u265F</span>`, //'\u2659',
-    "k": `<span class='black-piece'>\u265A</span>`,
-    "q": `<span class='black-piece'>\u265B</span>`,
-    "r": `<span class='black-piece'>\u265C</span>`,
-    "b": `<span class='black-piece'>\u265D</span>`,
-    "n": `<span class='black-piece'>\u265E</span>`,
-    "p": `<span class='black-piece'>\u265F</span>`
+    "K": `<text x='2' y='17' class='white-piece'>\u265A</text>`, // \u2654
+    "Q": `<text x='2' y='17' class='white-piece'>\u265B</text>`, //'\u2655',
+    "R": `<text x='2' y='17' class='white-piece'>\u265C</text>`, //'\u2656',
+    "B": `<text x='2' y='17' class='white-piece'>\u265D</text>`, //'\u2657',
+    "N": `<text x='2' y='17' class='white-piece'>\u265E</text>`, //'\u2658',
+    "P": `<text x='2' y='17' class='white-piece'>\u265F</text>`, //'\u2659',
+    "k": `<text x='2' y='17' class='black-piece'>\u265A</text>`,
+    "q": `<text x='2' y='17' class='black-piece'>\u265B</text>`,
+    "r": `<text x='2' y='17' class='black-piece'>\u265C</text>`,
+    "b": `<text x='2' y='17' class='black-piece'>\u265D</text>`,
+    "n": `<text x='2' y='17' class='black-piece'>\u265E</text>`,
+    "p": `<text x='2' y='17' class='black-piece'>\u265F</text>`
 }
 const PIECE_NAME_MAPPING: {[key: string]: string} = {
     "K": 'the white king',
@@ -177,7 +177,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     }
 
     addSpace(char: string): string {
-        return `<div class='box'><svg style='width: 100%; height: 100%;' viewBox='0 0 20 20'><text x='2' y='17'>${char}</text></svg></div>`;
+        return `<div class='box'><svg style='width: 100%; height: 100%;' viewBox='0 0 20 20'>${char}</svg></div>`;
     }
 
     buildRow(contents: string): string {
@@ -206,7 +206,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         fen = fen.substring(0, fen.indexOf(' '));
         let result = `---\n`;
         let lines = fen.split('/');
-        result += `<style>.play-area {width: 512px; min-width: 50%; padding-bottom: 100%; position: relative;}.chessboard {width: 100%; height: 100%; position: absolute; top: 0; left: 0; background: darkslategray} .row{width: 100%; height: 12.5%; display: flex;} div.box {width: 12.5%; height: 100%; display: flex; align-items: center; justify-content: center; font-size: 100%; font-family: monospace;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
+        result += `<style>.play-area {width: 50%; padding-bottom: 100%; position: relative;}.chessboard {width: 100%; height: 100%; position: absolute; top: 0; left: 0; background: darkslategray} .row{width: 100%; height: 12.5%; display: flex;} div.box {width: 12.5%; height: 100%; display: flex; align-items: center; justify-content: center; font-size: 100%; font-family: monospace;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
         result += `<div class='play-area'><div class='chessboard'>`;
         lines.forEach(line => result += this.buildRow(line));
         result += `</div></div>`;
