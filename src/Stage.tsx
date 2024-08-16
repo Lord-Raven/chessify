afbc984b2b7c78fbf6b37012f25397d973128650
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-16 13:10:25 -0400

    More formatting.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -206,7 +206,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         fen = fen.substring(0, fen.indexOf(' '));
         let result = `---\n`;
         let lines = fen.split('/');
-        result += `<style>.chessboard {width: 100%; padding-bottom: 0; background: darkslategray} div.box {width: 12.5%; height: 12.5%; display: flex; align-items: center; justify-content: center; font-size: 10%; font-family: monospace;}.row{display: flex;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
+        result += `<style>.chessboard {width: 100%; padding-bottom: 0; background: darkslategray} div.box {width: 12.5%; padding-bottom: 0; display: flex; align-items: center; justify-content: center; font-size: 10%; font-family: monospace;}.row{width: 100%; display: flex;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
         result += `<div class='chessboard'>`;
         lines.forEach(line => result += this.buildRow(line));
         result += `</div>`;
