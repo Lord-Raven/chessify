3a4823ac96f78c4b25b0b7ed54d691f53451d38b
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-16 10:51:30 -0400

    More formatting.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -14,18 +14,32 @@ type ChatStateType = any;
 const MOVE_REGEX = /([a-hA-H][1-8])/gm
 
 const PIECE_MAPPING: {[key: string]: string} = {
-    "K": '\u2654',
-    "Q": '\u2655',
-    "R": '\u2656',
-    "B": '\u2657',
-    "N": '\u2658',
-    "P": '\u2659',
-    "k": '\u265A',
-    "q": '\u265B',
-    "r": '\u265C',
-    "b": '\u265D',
-    "n": '\u265E',
-    "p": '\u265F'
+    "K": '<span class="white-piece">\u265A</span>', // \u2654
+    "Q": '<span class="white-piece">\u265B</span>', //'\u2655',
+    "R": '<span class="white-piece">\u265C</span>', //'\u2656',
+    "B": '<span class="white-piece">\u265D</span>', //'\u2657',
+    "N": '<span class="white-piece">\u265E</span>', //'\u2658',
+    "P": '<span class="white-piece">\u265F</span>', //'\u2659',
+    "k": '<span class="black-piece">\u265A</span>',
+    "q": '<span class="black-piece">\u265B</span>',
+    "r": '<span class="black-piece">\u265C</span>',
+    "b": '<span class="black-piece">\u265D</span>',
+    "n": '<span class="black-piece">\u265E</span>',
+    "p": '<span class="black-piece">\u265F</span>'
+}
+const PIECE_NAME_MAPPING: {[key: string]: string} = {
+    "K": 'the white king',
+    "Q": 'the white queen',
+    "R": 'a white rook',
+    "B": 'a white bishop',
+    "N": 'a white knight',
+    "P": 'a white pawn',
+    "k": 'the black king',
+    "q": 'the black queen',
+    "r": 'a black rook',
+    "b": 'a black bishop',
+    "n": 'a black knight',
+    "p": 'a black pawn'
 }
 export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateType, ConfigType> {
 
@@ -109,24 +123,22 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             }
 
             if (aiNote == '') {
-                console.log('Making a move');
                 // Must be a valid move; make it so.
+                aiNote = `${this.describeMove(coordinates["start"], coordinates["end"], "{{user}}", this.gameState)}`;
                 this.gameState = move(this.gameState, coordinates["start"], coordinates["end"]);
+
                 visualState = this.buildBoard();
-                console.log(visualState);
+                console.log(this.gameState);
 
                 // TODO: A big flaw of both turns occurring here is that the AI never sees the intermediate FEN and has limited insight into what the user's move accomplished; consider attempting to summarize for them.
 
                 // Then, make a move:
                 const charMove = aiMove(this.gameState, 2);
-                console.log(charMove);
-                console.log(Object.keys(charMove)[0]);
-                console.log(charMove[Object.keys(charMove)[0]]);
+                aiNote = `\nThen, ${this.describeMove(Object.keys(charMove)[0], charMove[Object.keys(charMove)[0]], "{{user}}", this.gameState)}`;
                 this.gameState = move(this.gameState, Object.keys(charMove)[0], charMove[Object.keys(charMove)[0]]);
-
-                aiNote = `{{user}} moved from ${coordinates["start"]} to ${coordinates["end"]}. {{char}} followed up by moving from ${Object.keys(charMove)[0]} to ${charMove[Object.keys(charMove)[0]]}.`
+                console.log(aiNote);
             } else {
-                console.log('Did not make a move (nothing legal');
+                console.log('Player did not input a legal move.');
             }
         } else {
             aiNote = `{{user}} didn't make a move this turn. {{char}} should spend some time chatting, bantering, or antagonizing them, but it will remain {{user}}'s turn.`;
@@ -134,7 +146,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
 
         return {
             stageDirections: this.replaceTags(
-                `[{{char}} and {{user}} are playing chess. Write a response describing the most recent move, including {{char}}'s reactions. ${aiNote}\nThis is the only event that has occurred, so make remarks about this move or the current state of the board. Additional moves will occur in future responses. Here is the board's FEN:\n${getFen(this.gameState)}]`,
+                `[{{char}} and {{user}} are playing chess. Write a response describing the most recent move, including {{char}}'s reactions. ${aiNote}\nThis is the only activity that has occurred, so focus on this, making remarks about these moves or the current state of the board. Additional moves will occur in future responses. Here is the board's FEN:\n${getFen(this.gameState)}]`,
                 {"user": this.user.name, "char": promptForId ? this.characters[promptForId].name : ''}),
             messageState: {gameState: JSON.stringify(this.gameState)},
             modifiedMessage: null,
@@ -154,6 +166,14 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             systemMessage: this.buildBoard(),
             chatState: null
         };
+    }
+
+    describeMove(start: string, end: string, playerTag: string, gameState: any) {
+        let description = `${playerTag} moves ${PIECE_NAME_MAPPING[gameState.pieces[start]]} from ${start} to ${end}`;
+        if (gameState.pieces[end]) {
+            description += `, capturing ${gameState.pieces[end]}`;
+        }
+        return `${description}.`;
     }
 
     addSpace(char: string): string {
@@ -186,7 +206,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         fen = fen.substring(0, fen.indexOf(' '));
         let result = `---\n`;
         let lines = fen.split('/');
-        result += `<style>.chessboard {padding: 6px; border: 1px solid #333; border-radius: 5px; display: inline-block; margin: auto; background: darkslategray} div.box {width: 50px; height: 50px; display: flex; align-items: center; justify-content: center; font-size: 32px; font-family: monospace; color: darkseagreen;}.row{display: flex;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;}</style>`;
+        result += `<style>.chessboard {padding: 6px; width: 100%; border: 1px solid #333; border-radius: 5px; display: inline-block; margin: auto; background: darkslategray} div.box {width: 12.5%; height: 12.5%; display: flex; align-items: center; justify-content: center; font-size: 10%; font-family: monospace;}.row{display: flex;} div.row:nth-child(odd) div.box:nth-child(odd){background: slategray;} div.row:nth-child(even) div.box:nth-child(even){background: slategray;} div.row:nth-child(even) div.box:nth-child(odd) {background: #333;} div.row:nth-child(odd) div.box:nth-child(even){background: #333;} .white-piece{ color: #fff;} .black-piece{ color: #000;}</style>`;
         result += `<div class='chessboard'>`;
         lines.forEach(line => result += this.buildRow(line));
         result += `</div>`;
