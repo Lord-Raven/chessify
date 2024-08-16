73b0a89deefd497995fec42e56667c870e8bc0a4
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-16 00:13:31 -0400

    Trying stuff out.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -180,9 +180,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         fen = fen.substring(0, fen.indexOf(' '));
         let result = `---\n`;
         let lines = fen.split('/');
-        result += `<style>div.box {width: 50px;height: 50px; display: flex; align-items: center; justify-content: center; font-size: 32px; font-family: monospace; color: darkseagreen;}`;
-        result += `.row{display: flex;}div.row:nth-child(odd)div.box:nth-child(odd),div.row:nth-child(even)div.box:nth-child(even) {background-color: #666666;}`;
-        result += `div.row:nth-child(even)div.box:nth-child(odd),div.row:nth-child(odd)div.box:nth-child(even) {background-color: transparent;}</style>`;
+        result += `<style>div.box {width: 50px;height: 50px; display: flex; align-items: center; justify-content: center; font-size: 32px; font-family: monospace; color: darkseagreen;}.row{display: flex;}div.row:nth-child(odd)div.box:nth-child(odd),div.row:nth-child(even)div.box:nth-child(even) {background-color: #666666;}div.row:nth-child(even)div.box:nth-child(odd),div.row:nth-child(odd)div.box:nth-child(even) {background-color: transparent;}</style>`;
         lines.forEach(line => result += this.buildRow(line));
 
         return `${result}`;
